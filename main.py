6beb603192611727a901259be45b00308df1995f
Danait Gebremedhin <danialex0194@gmail.com>
2023-05-17 11:29:24 -0700

    Tkinter

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+#Import the class library tkinter
+import tkinter as tk
+
+#Create a function that return an output
+def button_click():
+    print("Button clicked!")
+
+#Create a function that give title to the page
+root = tk.Tk()
+root.title("Button Example")    
+
+
+#Create an object with three parameter
+button =  tk.Button(root, text="Press it!", command=button_click)
+button.pack()
+
+#Create a mainloop to keep the rook window open
+root.mainloop()
+
 
